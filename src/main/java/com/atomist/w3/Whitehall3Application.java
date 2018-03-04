23d5a782c97c214cc4098dd4ffe7eae01f1facf6
Rod Johnson <johnsonroda@gmail.com>
2018-03-04 22:27:50 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/w3/Whitehall3Application.java
@@ -0,0 +1,12 @@
+package com.atomist.w3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Whitehall3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Whitehall3Application.class, args);
+	}
+}
---
+++ src/test/java/com/atomist/w3/Whitehall3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atomist.w3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Whitehall3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
